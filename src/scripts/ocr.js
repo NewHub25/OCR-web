fab8bf39771c459b1350cf4dea5dd6074e0ec537
Terry <master_correo144@hotmail.com>
2024-09-05 01:32:34 -0500

    OCR component created

--- src/scripts/ocr.js
+++ src/scripts/ocr.js
@@ -1,40 +1,37 @@
-import { createWorker } from "tesseract.js";
+import { createWorker } from 'tesseract.js';
 
-document
-  .getElementById("imageUploader")
-  .addEventListener("change", function (event) {
-    try {
-      console.group("Change event has dispatched");
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          console.group("READER loading");
-          const img = new Image();
-          img.src = e.target.result;
+document.getElementById('imageUploader').addEventListener('change', function (event) {
+  try {
+    console.group('Change event has dispatched');
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        console.group('READER loading');
+        const img = new Image();
+        img.src = e.target.result;
 
-          img.onload = async function () {
-            console.group("img loading");
-            const worker = await createWorker("eng", 1, {
-              logger: (m) => console.log(m), // Add logger here
-            });
-            const ret = await worker.recognize(img);
-            console.log(ret.data.text);
-            const {
-              data: { text },
-            } = ret;
-            document.getElementById("extractedText").innerText = text;
-            document.getElementById("status").innerText =
-              "Text extracted successfully!";
-            await worker.terminate();
-            console.groupEnd();
-          };
+        img.onload = async function () {
+          console.group('img loading');
+          const worker = await createWorker('eng', 1, {
+            logger: (m) => console.log(m),
+          });
+          const ret = await worker.recognize(img);
+          console.log(ret.data.text);
+          const {
+            data: { text },
+          } = ret;
+          document.getElementById('extractedText').innerText = text;
+          document.getElementById('status').innerText = 'Text extracted successfully!';
+          await worker.terminate();
           console.groupEnd();
         };
-        reader.readAsDataURL(file);
-      }
-      console.groupEnd();
-    } catch (error) {
-      console.log(error.message);
+        console.groupEnd();
+      };
+      reader.readAsDataURL(file);
     }
-  });
+    console.groupEnd();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
